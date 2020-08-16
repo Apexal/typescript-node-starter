52ff1986612391ec02db70ce890d74dac3a0607f
Frank Matranga <thefrankmatranga>
2020-08-15 23:09:58 -0400

    Improve logger format

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,7 +1,19 @@
-import { createLogger, transports } from 'winston'
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { createLogger, transports, format } from 'winston'
 
 const logger = createLogger({
-  transports: [new transports.Console()],
+  transports: [new transports.File({ filename: 'combined.log' })],
 })
+
+// Only log to console if not in production
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new transports.Console({
+      format: format.simple(),
+    }),
+  )
+}
 
 export default logger
