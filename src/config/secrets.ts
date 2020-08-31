b6797d6ad332b44927879ab8de34395ff335672d
Frank Matranga <thefrankmatranga@gmail.com>
2020-08-30 22:28:41 -0400

    Add TypeORM

--- src/config/secrets.ts
+++ src/config/secrets.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import assert from 'assert'
 
-const envKeys = <const>['MESSAGE'] // Add keys like 'PORT', 'SECRET', 'DATABASE_URL', etc.
+const envKeys = <const>[] // Add keys like 'PORT', 'SECRET', 'DATABASE_URL', etc.
 
 // Assert that the env keys are set
 for (const envKey of envKeys) {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import './config/secrets'
+import 'reflect-metadata'
 import logger from './config/logger'
+import './config/db'
 
 logger.info('Running project! Replace me with real code.')
